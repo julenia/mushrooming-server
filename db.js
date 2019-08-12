ebabf0a557214e9e7213a6644ae9efbf146f5147
julenia <julia.jankowska@onet.eu>
2019-08-12 18:12:08 +0200

    enabling heroku database option

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const databaseUrl = 'postgres://postgres:secret@localhost:5432/postgres'
+const databaseUrl = process.env.DATABASE||'postgres://postgres:secret@localhost:5432/postgres'
 const db = new Sequelize(databaseUrl)
 
 db
