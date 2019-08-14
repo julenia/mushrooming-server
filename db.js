07400e3777087d26c5d9f16520590324071edaef
julenia <julia.jankowska@onet.eu>
2019-08-14 18:14:23 +0200

    db force true

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const databaseUrl = process.env.DATABASE||'postgres://postgres:secret@localhost:
 const db = new Sequelize(databaseUrl)
 
 db
-  .sync({force: false})
+  .sync({force: true})
   .then(() => console.log('Database connected'))
   .catch(console.error)
 
